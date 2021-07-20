f8a5b7daa2e6453851832c1794c52f5afb3eb5e5
umme <unnipa01@yahoo.com>
2021-07-20 17:53:57 -0500

    initial copy

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CalAreaCrcl
+{
+    class Program
+    {
+        public static double AreaOfCircle(double radius)
+        {
+            
+            return (Math.PI * radius * radius);
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            double radius;
+            double AreaCir;
+
+            Console.WriteLine("What is the radius of the circle?");
+            radius = double.Parse(Console.ReadLine());
+            AreaCir = AreaOfCircle(radius);
+
+            Console.WriteLine($"The area of the circle is {AreaCir}");
+
+        }
+
+    }
+}
+
+                
